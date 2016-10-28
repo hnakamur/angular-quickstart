4741799a9a36cfe6c13dc576b0f7aa7e1668fe6c
Hiroaki Nakamura <hnakamur@gmail.com>
2016-10-28 16:38:01 +0900

    Add the hero editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
+export class Hero {
+  id: number;
+  name: string;
+}
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">,
+    </div>
+    `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
