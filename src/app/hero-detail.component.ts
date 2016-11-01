75a932171942fb419fcf63ccee368e976c84bd3e
Hiroaki Nakamura <hnakamur@gmail.com>
2016-11-01 10:51:54 +0900

    (WIP) Use Angular's HTTP service

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit }      from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location }               from '@angular/common';
 
-import { Hero }         from './hero';
-import { HeroService }  from './hero.service';
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
+
 @Component({
+  moduleId: module.id,
   selector: 'my-hero-detail',
   templateUrl: 'hero-detail.component.html',
   styleUrls: [ 'hero-detail.component.css' ]
@@ -26,10 +28,16 @@ export class HeroDetailComponent implements OnInit {
     });
   }
 
+  save(): void {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
   goBack(): void {
     this.location.back();
   }
 }
+
 
 /*
 Copyright 2016 Google Inc. All Rights Reserved.
